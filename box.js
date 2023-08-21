d6d433e04112c539db11077b60392f8a988a470f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-21 22:26:05 +0530

    first commit

---
+++ box.js
@@ -0,0 +1,18 @@
+function Box (x, y, w, h) {
+    this.body= Bodies.rectangle(x, y, w, h);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+
+    this.show = function() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rect(0, 0, this.w, this.h);
+
+        pop();
+
+    }
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+//module aliases
+
+var Engine = Matter.Engine
+var Render = Matter.Render
+var World = Matter.World
+var Bodies = Matter.Bodies;
+
+
+var engine;
+var world;
+var boxes = [];
+
+var ground;
+
+function setup() {
+    createCanvas(1200, 1200);
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine); 
+    ground = Bodies.rectangle(200, height, width, 10);    
+}
+
+function mousePressed() {
+    boxes.push(new Box(mouseX, mouseY, 30, 30));
+}
+
+function draw() {
+    background(0);
+    for(var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+}
