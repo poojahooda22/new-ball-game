b475218cb9d9b67847160f309419a8b18113bb6e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-22 21:01:40 +0530

    second

--- box.js
+++ box.js
@@ -1,8 +1,15 @@
-function Box (x, y, w, h) {
-    this.body= Bodies.rectangle(x, y, w, h);
-    this.body.friction = 1;
-    this.w = w;
-    this.h = h;
+function Circle (x, y, r) {
+
+    var options = {
+        friction: 0,
+        restitution: 0.2
+
+    }
+
+    this.body= Bodies.circle(x, y, r, options);
+    
+    this.r = r;
+    
     World.add(world, this.body);
 
     this.show = function() {
@@ -13,8 +20,10 @@ function Box (x, y, w, h) {
         translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
+        stroke(255);
+        fill(225,15,100);
 
-        rect(0, 0, this.w, this.h);
+        ellipse(0, 0, this.r * 2);
 
         pop();
 
