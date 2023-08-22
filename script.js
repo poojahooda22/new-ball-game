481eacbf851bbe901ccf255d2a6741810262d9d8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-22 21:01:56 +0530

    four

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ var Bodies = Matter.Bodies;
 
 var engine;
 var world;
-var boxes = [];
+var circles = [];
+var boundaries = [];
 
 var ground;
 
@@ -16,28 +17,34 @@ function setup() {
     createCanvas(1200, 1200);
     engine = Engine.create();
     world = engine.world;
-    Engine.run(engine); 
-    var options = {
-        isStatic: true
-    }
-    ground = Bodies.rectangle(600, height, width, 10, options); 
-    World.add(world, ground);   
+    // Engine.run(engine); 
+    
+    // boundaries.push(new Boundary(150, 100, width * 0.3, 20, 0.3));
+    // boundaries.push(new Boundary(250, 400, width * 0.6, 20, -0.3));
+
+    boundaries.push(new Boundary(650,600, width * 0.6, 20, -0.3));
+    boundaries.push(new Boundary(150,200, width * 0.6, 20, 0.3));
+    boundaries.push(new Boundary(250,1000, width * 0.6, 20, 0.3));
+
+    
+    // World.add(world, ground);   
 }
 
 function mouseDragged() {
-    boxes.push(new Box(mouseX, mouseY, 30, 30));
+    circles.push(new Circle(mouseX, mouseY, 30));
 }
 
 function draw() {
-    background(0);
-    
-    for(var i = 0; i < boxes.length; i++) {
-        boxes[i].show();
+    background(255);
+    Engine.update(engine);
+    for(var i = 0; i < circles.length; i++) {
+        circles[i].show();
     }
 
-    fill( 137, 207, 240);
-    noStroke();
-    rectMode(CENTER);
-    rect(600, height, width, 10);
+    for(var i = 0; i < boundaries.length; i++) {
+        boundaries[i].show();
+    }
+
+    
 
 }
